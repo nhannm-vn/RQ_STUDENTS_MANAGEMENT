f7730f05319b7d8d4b50193ebe27e99d50ae96ed
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-18 10:51:18 +0700

    update apis

---
+++ starter-template/src/apis/students.api.ts
@@ -0,0 +1,14 @@
+// file lưu các hàm call api
+// Vì json-server hỗ trợ phân trang nên mình truyền thêm 2 param là page và limit
+
+import { Students } from 'types/students.type'
+import http from 'utils/http'
+
+export const getStudents = (page: string | number, limit: string | number) => {
+  http.get<Students>('students', {
+    params: {
+      _page: page,
+      _limit: limit
+    }
+  })
+}
