284b89119528b5e231491ba4dfbcbb6ae3fe852b
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-20 23:06:39 +0700

    update previos button

--- starter-template/src/pages/Students/Students.tsx
+++ starter-template/src/pages/Students/Students.tsx
@@ -136,9 +136,18 @@ export default function Students() {
             <nav aria-label='Page navigation example'>
               <ul className='inline-flex -space-x-px'>
                 <li>
-                  <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
-                    Previous
-                  </span>
+                  {page === 1 ? (
+                    <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                      Previous
+                    </span>
+                  ) : (
+                    <Link
+                      to={`/students?page=${page - 1}`}
+                      className='rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                    >
+                      Previous
+                    </Link>
+                  )}
                 </li>
                 {/* Tạo cái mảng có số lượng phần tử totalPage để dễ dàng mmap ra */}
                 {Array(totalPage)
