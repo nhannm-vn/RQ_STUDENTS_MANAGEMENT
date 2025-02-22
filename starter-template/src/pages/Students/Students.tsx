d7492e8bb3e92775929f0c6fa3e3ab716659417f
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-22 07:26:33 +0700

    update button add student

--- starter-template/src/pages/Students/Students.tsx
+++ starter-template/src/pages/Students/Students.tsx
@@ -88,6 +88,16 @@ export default function Students() {
   return (
     <div>
       <h1 className='text-lg'>Students</h1>
+      {/* Chuẩn bị button để add student */}
+      <button
+        type='button'
+        className='me-2 mb-2 rounded-lg bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 px-5 py-2.5 text-center text-sm font-medium
+         text-white shadow-lg shadow-purple-500/50 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-purple-300 dark:shadow-lg
+         dark:shadow-purple-800/80 dark:focus:ring-purple-800'
+      >
+        Add Student
+      </button>
+
       {isLoading && (
         <div role='status' className='mt-6 animate-pulse'>
           <div className='mb-4 h-4  rounded bg-gray-200 dark:bg-gray-700' />
