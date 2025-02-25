a80c0f67c60a81d8091873194dac12667000e925
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-25 14:34:49 +0700

    update button

--- starter-template/src/pages/AddStudent/AddStudent.tsx
+++ starter-template/src/pages/AddStudent/AddStudent.tsx
@@ -301,12 +301,22 @@ export default function AddStudent() {
           </div>
         </div>
 
-        <button
-          type='submit'
-          className='w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto'
-        >
-          Submit
-        </button>
+        {isAddMode && (
+          <button
+            type='submit'
+            className='me-2 mb-2 rounded-lg bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-pink-300 dark:focus:ring-pink-800'
+          >
+            Add
+          </button>
+        )}
+        {!isAddMode && (
+          <button
+            type='submit'
+            className='me-2 mb-2 rounded-lg bg-gradient-to-r from-green-400 via-green-500 to-green-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800'
+          >
+            Update
+          </button>
+        )}
       </form>
     </div>
   )
