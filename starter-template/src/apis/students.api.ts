6730a1eff930ce7d36ca87ec3660f3cdcf62d042
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-25 13:40:25 +0700

    update api

--- starter-template/src/apis/students.api.ts
+++ starter-template/src/apis/students.api.ts
@@ -14,9 +14,20 @@ export const getStudents = (page: string | number, limit: string | number) =>
 
 // Viết method getStudent dựa vào id
 // Method này sẽ bỏ vào queryFn của useQuery
-export const getStudent = (id: string | number) => http.get<Student>(`students/${id}`)
+export const getStudent = (
+  id: string | number //
+) => http.get<Student>(`students/${id}`)
 
 // Viết riêng method dành riêng cho gọi http và post addStudent
 // Ở đây mình sẽ không có truyền cho nó id vì khi thêm mới một student thì id nó sẽ tự tạo cho mình
 //trên server database
-export const addStudent = (student: Omit<Student, 'id'>) => http.post<Student>('/students', student)
+export const addStudent = (
+  student: Omit<Student, 'id'> //
+) => http.post<Student>('/students', student)
+
+// Method giúp updateStudent
+// cần có id để nó biết dducc cần update thằng nào
+export const updateStudent = (
+  id: string | number,
+  student: Omit<Student, 'id'> //
+) => http.put<Student>(`students/${id}`, student)
