c5ed155542ae404ce7969bc07f5f34ab96d52c26
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-27 12:50:59 +0700

    update pause refetchOnWindowFocus

--- starter-template/src/index.tsx
+++ starter-template/src/index.tsx
@@ -8,7 +8,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
+// setup trong defaultOptions giúp cho khi chuyển tab thì sẽ không refetch lại api
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
